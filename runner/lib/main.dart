3fba2a980891dd9b859b0467f0086bcf08666519
韩承海 <374802597@qq.com>
2018-06-11 18:45:33 +0800

    说声再见 Flutter

--- runner/lib/main.dart
+++ runner/lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_driver/driver_extension.dart';
+
 import 'const/strings.dart';
 import 'project_list.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter_driver/driver_extension.dart';
 
 /* 
   Dart 应用是单线程的，但是 Dart 支持代码运行在其它线程上，同时也支持使用 async/await 模式让代码异步执行，而不会阻塞 UI 线程。
@@ -23,6 +23,7 @@ class AwesomeTips extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
       theme: ThemeData(
 //          brightness: Brightness.dark,
